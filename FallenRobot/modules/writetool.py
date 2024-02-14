8b8278313f68887e3880c69cf266e6ede274b102
NOBY DAYAN <87512608+Noby007@users.noreply.github.com>
2024-02-14 21:20:47 +0530

    Update writetool.py

--- FallenRobot/modules/writetool.py
+++ FallenRobot/modules/writetool.py
@@ -12,20 +12,20 @@ async def handwrite(_, message: Message):
             if len(message.command) < 3
             else message.text.split(None, 1)[1].replace(" ", "%20")
         )
-        m = await pgram.send_message(message.chat.id, "Wʀɪᴛɪɴɢ....")
+        m = await pgram.send_message(message.chat.id, "Wait panra paiya")
         photo = "https://apis.xditya.me/write?text=" + name
-        await pgram.send_photo(message.chat.id, photo=photo, caption="Wʀɪᴛᴛᴇɴ Bʏ ✍️ <a href='https://t.me/SmileHelp_Robot'>Sᴍɪʟᴇ</a>")         
+        await pgram.send_photo(message.chat.id, photo=photo, caption="Yeluthi kuduthathu ✍️ <a href='https://t.me/nobygod'>nan than</a>")         
         await m.delete()
     else:
         lol = message.reply_to_message.text
         name = lol.split(None, 0)[0].replace(" ", "%20")
-        m = await pgram.send_message(message.chat.id, "Wʀɪᴛɪɴɢ....")
+        m = await pgram.send_message(message.chat.id, "Wait panra paiya")
         photo = "https://apis.xditya.me/write?text=" + name
-        await pgram.send_photo(message.chat.id, photo=photo, caption="Wʀɪᴛᴛᴇɴ Bʏ ✍️ <a href='https://t.me/SmileHelp_Robot'>Sᴍɪʟᴇ</a>")         
+        await pgram.send_photo(message.chat.id, photo=photo, caption="Yeluthi kuduthathu ✍️ <a href='https://t.me/nobygod'>nan than</a>")         
         await m.delete()
 
 __mod_name__ = "WriteTool"
 __command_list__ = ["write"]
 __help__ = """
 ᴡʀɪᴛᴇs ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ ᴏɴ ᴡʜɪᴛᴇ ᴘᴀɢᴇ ᴡɪᴛʜ ᴀ ᴘᴇɴ 🖊.
-❍ /write <ᴛᴇxᴛ> *:* ᴡʀɪᴛᴇs ᴛʜᴇ ɢɪᴠᴇɴ ᴛᴇxᴛ. """
+❍ /write <ᴛᴇxᴛ> *:* noby na yeluthi tharen da. """
