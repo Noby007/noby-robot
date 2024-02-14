89f33fb5f7a57524c0b4619468420546a9bb185a
NOBY DAYAN <87512608+Noby007@users.noreply.github.com>
2024-02-14 21:49:15 +0530

    Update chatbot.py
    thapu panra da

--- FallenRobot/modules/chatbot.py
+++ FallenRobot/modules/chatbot.py
@@ -2,7 +2,6 @@ import html
 import json
 import re
 from time import sleep
-
 import requests
 from telegram import (
     CallbackQuery,
@@ -13,6 +12,7 @@ from telegram import (
     Update,
     User,
 )
+from telegram.error import BadRequest, RetryAfter, Unauthorized
 from telegram.ext import (
     CallbackContext,
     CallbackQueryHandler,
@@ -23,147 +23,128 @@ from telegram.ext import (
 from telegram.utils.helpers import mention_html
 
 import FallenRobot.modules.sql.chatbot_sql as sql
-from FallenRobot import BOT_ID, BOT_NAME, BOT_USERNAME, dispatcher
+from FallenRobot import BOT_ID, BOT_NAME, dispatcher
 from FallenRobot.modules.helper_funcs.chat_status import user_admin, user_admin_no_reply
+from FallenRobot.modules.helper_funcs.filters import CustomFilters
 from FallenRobot.modules.log_channel import gloggable
-
 
 @user_admin_no_reply
 @gloggable
-def fallenrm(update: Update, context: CallbackContext) -> str:
+def fallen_disable(update: Update, context: CallbackContext) -> str:
     query: Optional[CallbackQuery] = update.callback_query
     user: Optional[User] = update.effective_user
-    match = re.match(r"rm_chat\((.+?)\)", query.data)
-    if match:
-        user_id = match.group(1)
+    if match := re.match(r"rm_chat\((.+?)\)", query.data):
+        user_id = match[1]
         chat: Optional[Chat] = update.effective_chat
-        is_fallen = sql.set_fallen(chat.id)
-        if is_fallen:
-            is_fallen = sql.set_fallen(user_id)
+        if is_fallen := sql.rem_fallen(chat.id):
+            sql.rem_fallen(user_id)
             return (
                 f"<b>{html.escape(chat.title)}:</b>\n"
                 f"AI_DISABLED\n"
-                f"<b>Admin :</b> {mention_html(user.id, html.escape(user.first_name))}\n"
+                f"<b>Admin:</b> {mention_html(user.id, html.escape(user.first_name))}\n"
             )
         else:
             update.effective_message.edit_text(
-                "{} ᴄʜᴀᴛʙᴏᴛ ᴅɪsᴀʙʟᴇᴅ ʙʏ {}.".format(
-                    dispatcher.bot.first_name, mention_html(user.id, user.first_name)
-                ),
+                f"Hey Darling Fallen Chatbot disabled by {mention_html(user.id, user.first_name)}.",
                 parse_mode=ParseMode.HTML,
             )
 
     return ""
-
 
 @user_admin_no_reply
 @gloggable
-def fallenadd(update: Update, context: CallbackContext) -> str:
+def fallen_enable(update: Update, context: CallbackContext) -> str:
     query: Optional[CallbackQuery] = update.callback_query
     user: Optional[User] = update.effective_user
-    match = re.match(r"add_chat\((.+?)\)", query.data)
-    if match:
-        user_id = match.group(1)
+    if match := re.match(r"add_chat\((.+?)\)", query.data):
+        user_id = match[1]
         chat: Optional[Chat] = update.effective_chat
-        is_fallen = sql.rem_fallen(chat.id)
-        if is_fallen:
-            is_fallen = sql.rem_fallen(user_id)
+        if is_fallen := sql.set_fallen(chat.id):
+            sql.set_fallen(user_id)
             return (
                 f"<b>{html.escape(chat.title)}:</b>\n"
-                f"AI_ENABLE\n"
-                f"<b>Admin :</b> {mention_html(user.id, html.escape(user.first_name))}\n"
+                f"AI_ENABLED\n"
+                f"<b>Admin:</b> {mention_html(user.id, html.escape(user.first_name))}\n"
             )
         else:
             update.effective_message.edit_text(
-                "{} ᴄʜᴀᴛʙᴏᴛ ᴇɴᴀʙʟᴇᴅ ʙʏ {}.".format(
-                    dispatcher.bot.first_name, mention_html(user.id, user.first_name)
-                ),
+                f"Hey Darling Fallen Chatbot enabled by {mention_html(user.id, user.first_name)}.",
                 parse_mode=ParseMode.HTML,
             )
 
     return ""
-
 
 @user_admin
 @gloggable
 def fallen(update: Update, context: CallbackContext):
+    update.effective_user
     message = update.effective_message
-    msg = "• ᴄʜᴏᴏsᴇ ᴀɴ ᴏᴩᴛɪᴏɴ ᴛᴏ ᴇɴᴀʙʟᴇ/ᴅɪsᴀʙʟᴇ ᴄʜᴀᴛʙᴏᴛ"
+    msg = "Choose an option"
     keyboard = InlineKeyboardMarkup(
         [
-            [
-                InlineKeyboardButton(text="ᴇɴᴀʙʟᴇ", callback_data="add_chat({})"),
-                InlineKeyboardButton(text="ᴅɪsᴀʙʟᴇ", callback_data="rm_chat({})"),
-            ],
+            [InlineKeyboardButton(text="Enable", callback_data="add_chat({})")],
+            [InlineKeyboardButton(text="Disable", callback_data="rm_chat({})")],
         ]
     )
     message.reply_text(
-        text=msg,
+        msg,
         reply_markup=keyboard,
         parse_mode=ParseMode.HTML,
     )
-
 
 def fallen_message(context: CallbackContext, message):
     reply_message = message.reply_to_message
     if message.text.lower() == "fallen":
         return True
-    elif BOT_USERNAME in message.text:
-        return True
-    elif reply_message:
-        if reply_message.from_user.id == BOT_ID:
+    if reply_message:
+        if reply_message.from_user.id == context.bot.get_me().id:
             return True
     else:
         return False
-
 
 def chatbot(update: Update, context: CallbackContext):
     message = update.effective_message
     chat_id = update.effective_chat.id
     bot = context.bot
     is_fallen = sql.is_fallen(chat_id)
-    if is_fallen:
+    if not is_fallen:
         return
 
     if message.text and not message.document:
         if not fallen_message(context, message):
             return
+        Message = message.text
         bot.send_chat_action(chat_id, action="typing")
-        request = requests.get(
-            f"https://kora-api.vercel.app/chatbot/2d94e37d-937f-4d28-9196-bd5552cac68b/{BOT_NAME}/Anonymous/message={message.text}"
+        kukiurl = requests.get(
+            f"https://merissachatbot.tk/api/apikey=1985665341-MERISSArk4GRy9iD0/Neko/@Awesome-Prince/message={Message}"
         )
-        results = json.loads(request.text)
-        sleep(0.5)
-        message.reply_text(results["reply"])
 
+        Kuki = json.loads(kukiurl.text)
+        kuki = Kuki["reply"]
+        sleep(0.3)
+        message.reply_text(kuki, timeout=60)
 
-__help__ = f"""
-*{BOT_NAME} has an chatbot which provides you a seemingless chatting experience :*
+def list_all_chats(update: Update, context: CallbackContext):
+    chats = sql.get_all_kuki_chats()
+    text = "<b>Fallen Enabled Chats</b>\n"
+    for chat in chats:
+        try:
+            x = context.bot.get_chat(int(*chat))
+            name = x.title or x.first_name
+            text += f"    <code>{name}</code>\n"
+        except (BadRequest, Unauthorized):
+            sql.rem_fallen(*chat)
+        except RetryAfter as e:
+            sleep(e.retry_after)
+    update.effective_message.reply_text(text, parse_mode="HTML")
 
- »  /chatbot *:* Shows chatbot control panel
+__help__ = """
+*Admins only Commands*:
+      `/Chatbot`*:* Shows chatbot control panel
+  
+*Powered By @Programmer_Network*
 """
 
-__mod_name__ = "Cʜᴀᴛʙᴏᴛ"
+__mod_name__ = "ChatBot"
 
-
-CHATBOTK_HANDLER = CommandHandler("chatbot", fallen, run_async=True)
-ADD_CHAT_HANDLER = CallbackQueryHandler(fallenadd, pattern=r"add_chat", run_async=True)
-RM_CHAT_HANDLER = CallbackQueryHandler(fallenrm, pattern=r"rm_chat", run_async=True)
 CHATBOT_HANDLER = MessageHandler(
-    Filters.text
-    & (~Filters.regex(r"^#[^\s]+") & ~Filters.regex(r"^!") & ~Filters.regex(r"^\/")),
-    chatbot,
-    run_async=True,
-)
-
-dispatcher.add_handler(ADD_CHAT_HANDLER)
-dispatcher.add_handler(CHATBOTK_HANDLER)
-dispatcher.add_handler(RM_CHAT_HANDLER)
-dispatcher.add_handler(CHATBOT_HANDLER)
-
-__handlers__ = [
-    ADD_CHAT_HANDLER,
-    CHATBOTK_HANDLER,
-    RM_CHAT_HANDLER,
-    CHATBOT_HANDLER,
-]
