c671d5494e92affae5c58e51aab9a2c809b3030e
NOBY DAYAN <87512608+Noby007@users.noreply.github.com>
2024-02-14 21:30:37 +0530

    Update welcome.py

--- FallenRobot/modules/welcome.py
+++ FallenRobot/modules/welcome.py
@@ -181,7 +181,7 @@ def new_member(update: Update, context: CallbackContext):
             # Give the owner a special welcome
             if new_mem.id == OWNER_ID:
                 update.effective_message.reply_text(
-                    "Oh, Genos? Let's get this moving.", reply_to_message_id=reply
+                    "Oh,Noby ula va chello , presentaton ah ?.", reply_to_message_id=reply
                 )
                 welcome_log = (
                     f"{html.escape(chat.title)}\n"
@@ -193,7 +193,7 @@ def new_member(update: Update, context: CallbackContext):
             # Welcome Devs
             elif new_mem.id in DEV_USERS:
                 update.effective_message.reply_text(
-                    "Be cool! A member of the Heroes Association just joined.",
+                    "Macha Gold oda entry ya paru da.",
                     reply_to_message_id=reply,
                 )
                 welcome_log = (
